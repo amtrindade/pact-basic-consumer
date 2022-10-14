59d17924da46424f542a4d9d642415c70e0a3c00
Antonio Trindade <atrindade@umov.me>
2022-10-14 17:18:25 -0300

    Adiciona test strict

--- src/main/java/br/ce/wcaquino/consumer/tasks/service/TasksConsumer.java
+++ src/main/java/br/ce/wcaquino/consumer/tasks/service/TasksConsumer.java
@@ -1,6 +1,7 @@
 package br.ce.wcaquino.consumer.tasks.service;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.http.client.ClientProtocolException;
@@ -26,6 +27,19 @@ public class TasksConsumer {
 	}
 	
 	public Task saveTask(String task, String dueDate) throws ClientProtocolException, IOException {
+		Map<String, Object> response = helper.post(tasksURL + "/todo", 
+				String.format("{\"task\": \"%s\", \"dueDate\": \"%s\"}", task, dueDate));
+		if (response.keySet().size() == 0) return null;
+		
+		return new Task(Long.parseLong(response.get("id").toString()),
+				response.get("task").toString(), response.get("dueDate").toString());
+	}
+	
+	@SuppressWarnings( {"rawtypes", "unused"} )
+	public Task saveStrictTask(String task, String dueDate) throws ClientProtocolException, IOException {				
+		List tasks = helper.getAll(tasksURL + "/todo", "");
+		//verify tasks with the same name...
+		
 		Map<String, Object> response = helper.post(tasksURL + "/todo", 
 				String.format("{\"task\": \"%s\", \"dueDate\": \"%s\"}", task, dueDate));
 		if (response.keySet().size() == 0) return null;
